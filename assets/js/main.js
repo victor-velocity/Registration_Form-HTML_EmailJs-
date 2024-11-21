e498da96553adaf2e821087edf0d7ac8d788e0bc
Asama Victor <hyxenaetechnology@gmail.com>
2024-11-21 14:51:00 +0100

    html form using email js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,39 @@
+function sendMail() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm('service_mtc66xk', 'template_yijgnpb', '#contact-form')
+          .then(() => {
+              // console.log('SUCCESS!');
+              // alert("Message sent successfully!");
+              document.getElementById('feedback').innerHTML="Message sent successfully!";
+          }, (error) => {
+              // console.log('FAILED...', error);
+              // alert("Message sending failed! Try again!");
+              document.getElementById('feedback').innerHTML="Message sending failed! Try again!";
+          });
+  });
+}
 
-  const formData = new FormData(this);
-  const data = Object.fromEntries(formData.entries());
-  data.maritalstatus = formData.get("maritalstatus");
-  
+
+function sendMail() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      let params = {
+          user_name: "document.getElementById('user_name').value",
+          user_email: "document.getElementById('user_email').value",
+          message: "document.getElementById('message').value",
+      }
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm('service_mtc66xk', 'template_yijgnpb', 'params')
+          .then(() => {
+              // console.log('SUCCESS!');
+              // alert("Message sent successfully!");
+              document.getElementById('feedback').innerHTML="Message sent successfully!";
+          }, (error) => {
+              // console.log('FAILED...', error);
+              // alert("Message sending failed! Try again!");
+              document.getElementById('feedback').innerHTML="Message sending failed! Try again!";
+          });
+  });
+}
